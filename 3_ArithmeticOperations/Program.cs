494a93462a7bf1f1ca29e788dc5c06af20d634c7
Алексей <setgecko@gmail.com>
2023-04-13 22:54:45 +0300

    3 Арифметические операции

---
+++ 3_ArithmeticOperations/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _3_ArithmeticOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - * / %
+            float result;
+            int x, y;
+
+            x = 5;
+            y = 2;
+
+            result = Convert.ToSingle(x) / y;
+            // Convert.ToSingle(x) = covert to float
+            Console.WriteLine(result);
+
+            int timeInMinutes = 130;
+            int hour;
+            int minute;
+
+            hour = timeInMinutes / 60;
+            minute = timeInMinutes % 60;
+            
+            Console.WriteLine("hours: " + hour);
+            Console.WriteLine("minutes: " + minute);
+        }
+    }
+}
