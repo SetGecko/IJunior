c64144a604b355da23bbf3fe4c7a73115cc7b676
Алексей <setgecko@gmail.com>
2023-04-13 22:40:16 +0300

    Основы, переменные по курсу

---
+++ 2_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _2_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Основные типы
+            int money = 3;
+            string name = "Zhuzha";
+            float fff = 6.7f;
+            char c = 'A';
+            bool bl = true;
+        }
+    }
+}
