1ea82dc33e8eb954f490ab9364d6c841fbfb416c
Алексей <setgecko@gmail.com>
2023-05-01 18:40:49 +0300

    Конкатенация

---
+++ 5_Concatenation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _5_Concatenation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Привет";
+            string name = "Пуци";
+            string message = greeting + " " + name;
+            Console.WriteLine(message + ", рад тебя видеть!");
+
+            int age = 5;
+            Console.WriteLine("Привет, завтра тебе исполнится: " + (age + 1));
+        }
+    }
+}
