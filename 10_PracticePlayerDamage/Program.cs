292ca258e65f808b5d167555da6abf023a656858
Алексей <setgecko@gmail.com>
2023-05-01 20:19:38 +0300

    Практика расчёт урона игроку

---
+++ 10_PracticePlayerDamage/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _10_PracticePlayerDamage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float health;
+            int armor;
+            int damage;
+            int percentConverter = 100;
+
+            Console.Write("Введите количество здоровья: ");
+            health = Convert.ToInt32(Console.ReadLine());   
+            
+            Console.Write("Введите количество брони: ");
+            armor = Convert.ToInt32(Console.ReadLine());   
+            
+            Console.Write("Введите количество урона: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            health -= damage * armor / percentConverter;
+            // health = 100 - ((50 * 50) / 100)
+
+            Console.WriteLine($"Вам нанесли {damage} урона. " +
+                $"У вас осталось {health} здоровья");
+
+
+
+        }
+    }
+}
