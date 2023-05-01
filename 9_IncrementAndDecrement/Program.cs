8cfd76b692664a7bb46dd1fded7b9873f216b178
Алексей <setgecko@gmail.com>
2023-05-01 19:31:29 +0300

    Инкремент и декремент

---
+++ 9_IncrementAndDecrement/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _9_IncrementAndDecrement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            i++;
+            Console.WriteLine(i);
+            i--;
+            Console.WriteLine(i);
+        }
+    }
+}
