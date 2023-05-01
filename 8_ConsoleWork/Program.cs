8219a411df95d8131960b57466e74c64b95db2a7
Алексей <setgecko@gmail.com>
2023-05-01 19:24:16 +0300

    Ввод и вывод на консоль

---
+++ 8_ConsoleWork/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _8_ConsoleWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Ваше имя: {name}");
+
+            int age;
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш возраст {age}");
+        }
+    }
+}
