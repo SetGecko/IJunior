41f4f0b60a00e3f0384bb3b157f326fe142e5712
Алексей <setgecko@gmail.com>
2023-05-01 18:46:43 +0300

    Интерполяция

---
+++ 6_Interpolation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _6_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 5;
+            string name = "Пуци";
+
+            Console.WriteLine("Ваше имя: " + name + " вам " + age + " лет!");
+            Console.WriteLine($"Ваше имя: {name} вам {age} лет!");
+        }
+    }
+}
